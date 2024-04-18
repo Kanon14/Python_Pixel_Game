0088a13727e9244e39b1a781be47b00ddabdc700
Kanon14 <geochua144@gmail.com>
2024-04-18 23:55:28 +0800

    overworld animation added

--- code/sprites.py
+++ code/sprites.py
@@ -1,3 +1,4 @@
+from typing import Any
 from settings import *
 
 class Sprite(pygame.sprite.Sprite):
@@ -5,3 +6,15 @@ class Sprite(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = surf
         self.rect = self.image.get_frect(topleft = pos) # get the floating point rect (new in pygame-ce)
+        
+class AnimatedSprite(Sprite):
+    def __init__(self, pos, frames, groups):
+        self.frame_index, self.frames = 0, frames
+        super().__init__(pos, frames[self.frame_index], groups)
+        
+    def animate(self, dt):
+        self.frame_index += ANIMATION_SPEED * dt
+        self.image = self.frames[int(self.frame_index % len(self.frames))]
+        
+    def update(self, dt):
+        self.animate(dt)
