adf1895126e0c26bc9fd289ccbff33d54c211bc0
Kanon14 <geochua144@gmail.com>
2024-04-26 23:03:22 +0800

    menu list, round-cornered selection added

---
+++ code/monster.py
@@ -0,0 +1,10 @@
+from game_data import MONSTER_DATA
+
+class Monster:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+        
+        # stats
+        self.element = MONSTER_DATA[name]['stats']['element']
+        self.base_stats = MONSTER_DATA[name]['stats']
