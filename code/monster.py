973b34f55e84755b046a6b4fddde0bc693f23bf9
Kanon14 <geochua144@gmail.com>
2024-05-01 23:28:44 +0800

    monster index completed

--- code/monster.py
+++ code/monster.py
@@ -13,6 +13,7 @@ class Monster:
         self.energy = self.base_stats['max_energy'] * self.level
         self.health -= randint(0, 200)
         self.energy -= randint(0, 100)
+        self.abilities = MONSTER_DATA[name]['abilities']
         
         # experience
         self.xp = randint(0, 1000)
@@ -20,3 +21,17 @@ class Monster:
         
     def get_stat(self, stat):
         return self.base_stats[stat] * self.level
+    
+    
+    def get_stats(self):
+        return {
+            'health': self.get_stat('max_health'),
+            'energy': self.get_stat('max_energy'),
+            'attack': self.get_stat('attack'),
+            'defense': self.get_stat('defense'),
+            'speed': self.get_stat('speed'),
+            'recovery': self.get_stat('recovery')
+        }
+        
+    def get_abilities(self):
+        return [ability for lvl, ability in self.abilities.items() if self.level >= lvl]
